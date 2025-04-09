96cca4fd57d6a895644496b2cf3b6dc46dee137e
noah <rauterberg@campus.tu-berlin.de>
2025-04-09 09:09:41 +0200

    add copyright to circle script

--- circle.py
+++ circle.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2025 Noah Rauterberg. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
 import matplotlib.pyplot as plt
 import numpy as np
 
